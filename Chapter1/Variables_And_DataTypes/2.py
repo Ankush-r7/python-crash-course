8e5cff93b1fab6e7b01aa8328b87d1df51c8c824
Ankush Rawat <rawatanku291@gmail.com>
2026-09-03 14:58:12 +0530

    Variable

---
+++ Chapter1/Variables_And_DataTypes/2.py
@@ -0,0 +1,20 @@
+# now let's see one more thing in python
+
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+
+# here the first line will print
+
+# Hello Python world!
+
+# And the second line will print 
+
+# Hello Python Crash Course world!
+
+# As the name of the variables are same but the python will always print last value stored inside the varible
+
+# This means we can update
---
+++ Chapter1/Variables_And_DataTypes/variables.py
@@ -0,0 +1,13 @@
+# In this we are going to learn about the variables
+
+message = "Hello Python World"
+
+print(message)
+
+
+# here message is variable 
+
+# every variable holds a value which is the information associated with that variable
+
+# in our case the value is text "Hello Python World"
+
