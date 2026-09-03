0fac89163c8e2c05cd33f4721c9253fd774e2c8c
Ankush Rawat <rawatanku291@gmail.com>
2026-09-03 13:50:14 +0530

    Introduction

---
+++ Chapter1/1.py
@@ -0,0 +1,34 @@
+# Hey Everyone 
+# Myself Ankush Rawat and today we are going to learn about the Introduction part of our book
+# Maybe it seems a little boring to you but let's understand some of the important things mentioned in this book 
+
+# In the introduction part the writer is just saying about the how he wrote his first program 
+
+# Then the author is telling for who this book is for
+
+
+# In the first part of the book you will some of tha basic concepts of programming language
+
+# So this book is for everyone who wants to learn python
+
+# The book is divided into two parts
+
+# First part is just of basic understanding or different concepts
+
+# Then in the second part we will be creating some projects 
+
+
+
+# Now the question is why python
+
+# There are many reasons to choose python
+
+# The first is it's simplicity
+# We don't have to write a very large amount of code to do a simple task 
+# then it is also easy to read and understand
+
+# Then also it is used in various fields 
+
+# and there are many more things because of which you should understand python and learn it
+
+
