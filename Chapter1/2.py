5f8db7b6be96c21ace40180a1efcf610376f8792
Ankush Rawat <rawatanku291@gmail.com>
2026-09-03 14:39:06 +0530

    Running first program

---
+++ Chapter1/2.py
@@ -0,0 +1,12 @@
+# Now we will start the part one of the book
+
+# That is getting started
+
+# in this we will be creating our first python program that is hello world
+
+# we need to install python in our computer and also text editor like vs code
+
+# we will learn how to setup our python programming environment to learn it
+
+# in this whole book you will see code snippets
+
---
+++ Chapter1/first.py
@@ -0,0 +1,5 @@
+# Now we will be writing are first python program that is hello world
+
+
+
+print("Hello World")
