8ed99a79a7ac5f673775504c14de69fe875a08e0
Ankush Rawat <rawatanku291@gmail.com>
2026-09-06 12:26:48 +0530

    Dictionary in Python

--- Chapter6/dictionary.py
+++ Chapter6/dictionary.py
@@ -1,3 +1,94 @@
 # Today, we are going to learn about another data structure in Python
 # Dictionary
 
+
+# Simple Dictionary
+
+alien_0 = {
+    'color' : 'green',
+    'points' : 5
+}
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+
+# Working with Dictionary
+
+# A dictionary in Python is a collection of key value pairs
+
+# Each key is connected to a value
+
+# A key value pair is a set of values associated with each other
+
+# Adding new key value pair in the dictionary
+
+print(alien_0)
+
+alien_0['x_position'] = 0
+
+alien_0['y_position'] = 25
+
+print(alien_0)
+
+
+# Starting with an empty dictionary
+
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
+
+
+
+# Modiffying values in a dictionary
+
+alien_0 = {'color' : 'green'}
+
+print("The alien is "+alien_0['color'] + ".")
+
+alien_0['color'] = 'yellow'
+
+print("The alien is now "+alien_0['color'] + ".")
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("Original x-position: " + str(alien_0['x_position']))
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # This must be a fast alien.
+    x_increment = 3
+    # The new position is the old position plus the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print("New x-position: " + str(alien_0['x_position']))
+
+
+# Removin Key value pair
+
+alien_0 = {'color' : 'green','points' : 5}
+
+print(alien_0)
+
+del alien_0['points']
+
+print(alien_0)
+
+"Be aware that the deleted key-value pair is removed permanently."
+
+# A Dictionary of Similar Objects
+
+
+favorite_languages = {
+    'jen' : 'python',
+    'sarah' : 'c',
+    'edward' : 'ruby',
+    'phil' : 'python'
+}
+
+print("Sarah's favorite language is "+favorite_languages['sarah'].title() + ".")
